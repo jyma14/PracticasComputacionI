2727785f2e25d9f854b5dfcf23e026c2217fec47
jyma14 <89822203+jyma14@users.noreply.github.com>
2021-10-15 07:04:49 -0500

    Update Punto.cpp

--- Puntos/Punto.cpp
+++ Puntos/Punto.cpp
@@ -5,49 +5,47 @@
  * October 13, 2021
  */
 
-#incluide <iostream>
-#incluide <math.h>
-#define PI 3.141592
-#incluide "Punto.h"
+#include <iostream>
+#include <math.h>
+#define PI 3.14159265
+#include "Punto.h"
 
 using namespace std;
 
-Punto2D: :Punto2D() {
-	a = 0;
-	b = 0;
-	c = 0;
+Punto2D::Punto2D() {
+    a = 0;
+    b = 0;
+    c = 0;
 }
 
- void Punto2D: :SetPosition(float x, float y) {
- 	a = x;
- 	b = y;
- }
-
- void Punto2D: :Trasladar(float temp1, float temp2) {
- 	a = a + temp1;
- 	b = b + temp2;
+void Punto2D::SetPosicion(float x,float y){
+    a = x;
+    b = y;
 }
 
-void Punto2D: :RotarRespectoAlOrigen(float temp1){
-	c = a;
-	a = c*cos(temp1*PI/180) - b*sen(temp1*PI/180);
-	b = c*sen(temp1*PI/180) + b*cos(temp1*PI/180);
+void Punto2D::Trasladar(float temp1, float temp2) {
+    a = a + temp1;
+    b = b + temp2;
 }
 
-void Punto2D: :Escalar(float temp1, float temp2){
-	a = a*temp1;
-	b = b*temp2;
+void Punto2D::RotarRespectoAlOrigen(float temp1){
+    c = a;
+    a = c*cos(temp1*PI/180) - b*sen(temp1*PI/180);
+    b = c*sen(temp1*PI/180) + b*cos(temp1*PI/180);
 }
 
-float Punto2D: :GetX(){
-	return a;
+void Punto2D::Escalar(float temp1, float temp2) {
+    a = a*temp1;
+    b = b*temp2;
 }
 
-float Punto2D: :GetY(){
-	return b;
+float Punto2D::GetX() {
+    return a;
 }
 
-
+float Punto2D::GetY() {
+    return b;
+}
 
 
 
