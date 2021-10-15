e4b11223374f74b1df25abd4dfe575496d44e9b2
jyma14 <89822203+jyma14@users.noreply.github.com>
2021-10-15 07:01:21 -0500

    Update Punto.h

--- Puntos/Punto.h
+++ Puntos/Punto.h
@@ -4,25 +4,25 @@
  * October 13, 2021
  */
 
-#ifdef PUNTOS_PUNTOS_H
-#define PUNTOS_PUNTOS_H
+#ifndef PUNTOS_PUNTO_H
+#define PUNTOS_PUNTO_H
 #include <vector>
 
 using namespace std;
 
-class Punto2D;
+class Punto2D {
 public:
-	Punto2D;
-	void SetPosition(float x, float y);
-	void Trasladar(float temp1, float temp2);
-	void RotarRespectoAlOrigen(float temp1);
-	void Escalar(float temp1, float temp2);
-	float GetX();
-	float GetY();
+    Punto2D();
+    void SetPosicion(float x, float y);
+    void Trasladar(float temp1, float temp2);
+    void RotarRespectoAlOrigen(float temp1);
+    void Escalar(float temp1, float temp2);
+    float GetX();
+    float GetY();
 
 private:
-	float a, b, c;
+    float a, b, c;
 };
 
-// PUNTOS_PUNTOS_H
-#endif 
+
+#endif //PUNTOS_PUNTO_H
